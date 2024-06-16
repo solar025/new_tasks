1262729074901709df58e150ced165b266140292
archmight <35964960+archmight@users.noreply.github.com>
2024-06-16 14:08:03 +0300

    final

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import  java.lang.Object;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        createBackup("C:\\Users\\workstation\\Documents\\qt\\sql");
+
+        byte a[] = {-128+255, -128+30, -128};
+
+
+        int i = a[0]+128;
+        int j = a[1]+128;
+        int k = a[2]+128;
+
+        ArrayList<String> Field = new ArrayList<>();
+
+        for (int t = 9; t > 0; t--) {
+            if (i > j & i > k) {
+                i -= t*t;
+                Field.add(0, "X");
+            }
+            else if (j > i & j > k) {
+                j -= t*t;
+                Field.add(0, "0");
+            }
+            else {
+                k -= t*t;
+                Field.add(0, " ");
+            }
+        }
+
+        for (int l = 0; l < 3; l++) {
+            System.out.println(Field.get(l * 3) + Field.get(1 + l*3) + Field.get(2 + l*3));
+        }
+    }
+
+    public static void createBackup(String path){
+
+        File source = new File(path);
+        File dest = new File(path+"\\backup");
+        new File(path+"\\backup").mkdirs();
+        try {
+            FileUtils.copyDirectory(source, dest);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
